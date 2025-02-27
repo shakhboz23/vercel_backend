5e6033e73e76c5e0b72620ddbd5723df709de237
shahbozhoca <upwork02092005@gmail.com>
2025-02-27 20:35:25 +0500

    .

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ async function bootstrap() {
       .addBearerAuth()
       .build();
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document, {
+    SwaggerModule.setup('/', app, document, {
       swaggerOptions: {
         docExpansion: 'none', // collapse the dropdown by default
       },
