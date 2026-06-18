150f9b2558059078c2b9a2fee81e378105ef7041
shahbozmamatkarimov <shahbozmamatkarimov2303@gmail.com>
2026-06-18 10:24:11 +0500

    .

--- src/utils/webAppInitData.ts
+++ src/utils/webAppInitData.ts
@@ -1,30 +1,25 @@
 import crypto from 'crypto';
 
 export function validateTelegramWebAppData(initData, botToken) {
-    // const params = new URLSearchParams(initData);
+    const params = new URLSearchParams(initData);
 
-    const hash = initData.hash;
-    delete initData.hash;
+    const hash = params.get('hash');
+    params.delete('hash');
 
-    const dataCheckString = [...initData.entries()]
+    const dataCheckString = [...params.entries()]
         .sort(([a], [b]) => a.localeCompare(b))
         .map(([key, value]) => `${key}=${value}`)
         .join('\n');
-    console.log(dataCheckString);
 
     const secretKey = crypto
         .createHmac('sha256', 'WebAppData')
         .update(botToken)
         .digest();
-    console.log(secretKey);
-
 
     const calculatedHash = crypto
-        .createHmac('sha256', secretKey as crypto.BinaryLike)
+        .createHmac('sha256', secretKey as crypto.BinaryLike )
         .update(dataCheckString)
         .digest('hex');
-    console.log(calculatedHash);
-
 
     return calculatedHash === hash;
 }
