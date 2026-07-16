a40c8cfabc41f08998f5fcb666a19bf8d86ea2b2
shahbozmamatkarimov <shahbozmamatkarimov2303@gmail.com>
2026-07-16 11:15:03 +0500

    .

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -544,7 +544,6 @@ export class UserService {
                           },
                         },
                         limit: 6,
-                        required: false,
                       },
                     ],
                   },
